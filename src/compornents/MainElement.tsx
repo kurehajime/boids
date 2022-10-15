3c00aca5f55ba3b72d718e38538ecdeb69d6032f
kurehajime <xiidec@gmail.com>
2022-10-15 19:42:27 +0900

    Initial commit

---
+++ src/compornents/MainElement.tsx
@@ -0,0 +1,10 @@
+import "./MainElement.css";
+
+export default function MainElement() {
+
+    return (
+        <div className='main'>
+            <h1>Hello</h1>
+        </div >
+    )
+}
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    base: "./",
+    server: {
+        open: true
+    },
+    build: {
+        outDir: './docs'
+    },
+    assetsInclude: ['**/*.png'],
+    plugins: [react()],
+})
